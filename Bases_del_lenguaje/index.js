e2025ea43e838d64b9c038f3dbbda71bd72a054c
John Pereira <john.epm1995@gmail.com>
2018-02-08 15:56:46 -0300

    triangulo ejercicio 1

---
+++ Bases_del_lenguaje/index.js
@@ -0,0 +1,30 @@
+//let de por si es una variable
+let base = 5;
+let altura = 7;
+//variable que no puede cambiar, no se le puede dar otro valor
+const num = 1;
+
+/* funcion anonima, pero que nunca se podra llamar
+function() {
+
+}
+*/
+//distintas formas de definir funciones en javascript:
+/*
+function triangleArea(base, altura){
+    console.log(`El area de un triangulo de base 
+    ${base} y altura ${altura} es: ${base * altura / 2}`);
+}
+*/
+
+/*
+let triangleArea = function(base, altura){
+    console.log(`El area de un triangulo de base 
+    ${base} y altura ${altura} es: ${base * altura / 2}`);
+}
+*/
+//arrow functions:
+let triangleArea = (base, altura) => {
+    console.log(`El area de un triangulo de base 
+    ${base} y altura ${altura} es: ${base * altura / 2}`);
+}
